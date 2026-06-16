d8fdb578043ea858017e57b5103100e6a7ea2547
Rajagopal807 <giri807@gmail.com>
2026-06-16 14:17:33 +0530

    Commited on 16-Jun-2026

--- AttendSync.WebApi/Program.cs
+++ AttendSync.WebApi/Program.cs
@@ -48,15 +48,12 @@ builder.Services.AddCors(opt =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AttendSync v1");
-        c.RoutePrefix = string.Empty; // Swagger at root
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AttendSync v1");
+    c.RoutePrefix = string.Empty; // Swagger at root
+});
 
 app.UseCors();
 app.UseAuthorization();
